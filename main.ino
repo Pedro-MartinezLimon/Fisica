84be9a96b5b398f4a32a5d5b49260302abafe468
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-10-23 19:00:52 -0600

    update: Con beeper

--- main.ino
+++ main.ino
@@ -1,7 +1,5 @@
 #include "HC_SR04.h"
 
-
-
 #define U_SENSOR_CM 0
 #define U_SENSOR_IN 1
 
@@ -11,14 +9,21 @@ uint8_t veces = 0;
 void setup() 
 {
   Serial.begin(9600);
+  pinMode(6, OUTPUT);
 }
 
 void loop() 
 {
   Serial.println("-------------------");
-  Serial.print("Distancia medida = ");
+  Serial.print("Distancia medida (cm) = ");
   Serial.print(sensor_ultrasonico.read(4, 5));
-  Serial.println(" cm");
-  Serial.println();
+  
+  //Verificación : Se puede modificar la distancia minima
+  if(sensor_ultrasonico.read(4,5) <= 15.0)
+    digitalWrite(6, HIGH);
+  else
+    digitalWrite(6, LOW);
+
+  
   delay(500);
 }
